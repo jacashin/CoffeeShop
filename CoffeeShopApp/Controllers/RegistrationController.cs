42b3f8aa9b47fcbff93ddc8d206b9c90785d0130
Jacob <jacobashin@gmail.com>
2019-07-15 18:20:48 -0400

    first commit

---
+++ CoffeeShopApp/Controllers/RegistrationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShopApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+       
+        public IActionResult RegisterView(string UserName)
+        {
+            var Coffee = new Registration
+            {
+                UserName = "",
+                email = "",
+                Password = "",
+                Password2 = ""
+            };
+
+            return View(UserName);
+        }
+
+        //[HttpPost]
+        public IActionResult FinishRegister(string UserName)
+        {
+            //var UserInfo = (Request.Form[UserName]).ToString();
+
+            //ViewData["UserInfo"] = UserInfo;
+
+            return View(UserName);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+        
+}
---
+++ CoffeeShopApp/Models/Registration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopApp.Models
+{
+    public class Registration
+    {
+        [Required (ErrorMessage = "Please enter a name")]
+        [MaxLength(24)]
+        [MinLength(4)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please a valid email address")]
+        public string email { get; set; }
+
+        [Required]
+        [MaxLength(24)]
+        [MinLength(4)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(24)]
+        [MinLength(4)]
+        [Compare("Password")]
+        public string Password2 { get; set; }
+    }
+}
